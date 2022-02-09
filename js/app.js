6c4217045b18bb684e5d4c4518e7835e70df365a
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-08 23:31:29 -0800

    edited functions for console, while, case, and comment outs

--- js/app.js
+++ js/app.js
@@ -1,97 +1,77 @@
 function userName() {
-  let name = prompt('Hello! What\'s your name?').toUpperCase();
-  while(name === null) {
-    name = prompt('Hello! What\'s your name?').toUpperCase();
+  let name = prompt('Hello! What\'s your name?');
+  name = name.toUpperCase();
+  while (name === '' || name === null) {
+    name = prompt('Sorry. You must enter your name.');
+    name = name.toUpperCase();
   }
-  if (name === "userName") {
-    console.log('User entered their name.')
-    alert('Welcome ${name}!');
-
-  }
-  else if (userName === 'null') {
-    alert('Sorry. You must enter your name, and it must be in UPPER case.');
-  }
+  name = name.toUpperCase();
+  // console.log('User entered their name: ' + name);
+  alert('Welcome ' + name + '!');
 }
 
 function userVehicle() {
-  let haveVehicle = prompt('Do you like your vehicle?').toLowerCase();
-  while(haveVehicle === null) {
-    haveVehicle = prompt ('Do you like your vehicle?').toLowerCase();
+  let haveVehicle = prompt('Do you have a vehicle?');
+  haveVehicle = haveVehicle.toLowerCase();
+  // console.log(haveVehicle);
+  while (haveVehicle != 'y' && haveVehicle != 'n') {
+    haveVehicle = prompt('Sorry. You must enter a "y" or "n".');
+    haveVehicle = haveVehicle.toLowerCase();
   }
-  if (haveVehicle === "y" || haveVehicle === "n") {
-    console.log('User told me if they like their vehicle.')
-    alert('I like mine, ${name}.');
-  }
-  else if (userVehicle === 'null') {
-
-    alert('Invalid input. You have to type y or n in lower case.');
-  }
+  // console.log('User told me if they like their vehicle.')
+  alert('I have a vehicle.');
 }
 
 function vehicleColor() {
-  let color = prompt('Do you have a light-colored vehicle?').toLowerCase();
-  while(color === null) {
-    color = prompt ('Do you have a light-colored vehicle?').toLowerCase();
+  let color = prompt('Do you have a light-colored vehicle?');
+  color = color.toLowerCase();
+  // console.log(color);
+  while (color != 'y' && color != 'n') {
+    color = prompt('Sorry. You must enter a "y" or "n".');
+    color = color.toLowerCase();
   }
-  if (color === "y" || color === "n") {
-    console.log('User replied on what color vehicle they have.')
 
-    alert('Invalid input. You have to type y or n in lower case.');
-  }
-  else if (vehicleColor === 'null') {
+  // console.log('User replied on what color vehicle they have.')
+  alert('I have a dark blue car.');
 
-    alert('My car is dark blue.');
-  }
 }
 
 function newVehicle() {
-  let vehicleAge = prompt('Do you have a new vehicle?').toUpperCase();
-  while(vehicleAge === null) {
-    vehicleAge = prompt ('Do you have a new vehicle?').toUpperCase();
+  let vehicleAge = prompt('Do you have a new vehicle?');
+  vehicleAge = vehicleAge.toLowerCase();
+  // console.log(vehicleAge);
+  while (vehicleAge != 'y' && vehicleAge != 'n') {
+    vehicleAge = prompt('Sorry. You must enter a "y" or "n".');
+    vehicleAge = vehicleAge.toLowerCase();
   }
-  if (vehicleAge === "y" || vehicleAge === "n") {
-    console.log('User replied if they have a new vehicle.')
 
-    alert('Invalid input. You have to type y or n in UPPER case.');
+  // console.log('User replied if they have a new vehicle.')
+  alert('My vehicle is 5 years old.');
 
-  }
-  else if (newVehicle === 'null') {
-
-    alert('My car is 5 years old.');
-  }
 }
 
 function likeVehicle() {
-  let like = prompt('Do you like your vehicle?').toLowerCase();
-  while(like === null) {
-    like = prompt ('Do you like your vehicle?').toLowerCase(); 
+  let like = prompt('Do you like your vehicle?');
+  like = like.toLowerCase();
+  // console.log(like);
+  while (like != 'y' && like != 'n') {
+    like = prompt('Sorry. You must enter a "y" or "n".');
+    like = like.toLowerCase();
   }
-  if (like === "y" || like === "n") {
-    console.log('User answered if they like their vehicle.')
-
-    alert('Invalid input. You have to type y or n in lower case.');
-
-  }
-  else if (likeVehicle === 'null') {
-
-    alert('I love my car.');
-  }
+  // console.log('User answered if they like their vehicle.')
+  alert('I like mine.');
 }
 
 function dreamVehicle() {
-  let dream = prompt('Do you have a dream vehicle?').toUpperCase();
-  while(dream === null) {
-    while = prompt ('Do you have a dream vehicle?').toUpperCase();
+  let dream = prompt('Do you have a dream vehicle?');
+  dream = dream.toLowerCase();
+  // console.log(dream);
+  while (dream != 'y' && dream != 'n') {
+    dream = prompt('Sorry. You must enter a "y" or "n".');
+    dream = dream.toLowerCase();
   }
-  if (dream === "y" || dream === "n") {
-    console.log('User answered if they have a dream vehicle.')
-
-    alert('Invalid input. You have to type y or n in UPPER case.');
-
-  } else if (dreamVehicle === 'null') {
-
-    alert('My dream car is a hover car. Teehee! Thanks for playing my guessing game.');
-  }
+  // console.log('User answered if they have a dream vehicle.')
+  alert('My dream car is a hover car. Hahaha!');
 }
 
 let name = userName();
