f429928703d8170a2f6f84f11705c04977aec1fb
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-08 17:19:19 -0800

    added upper and lower case functions to code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 function userName() {
-  let name = prompt ('Hello! What\'s your name?');
+  let name = prompt ('Hello! What\'s your name?').toUpperCase();
   if(name ==="userName")
 
     alert('Welcome ${name}!');
 
   else
 
-    alert('Sorry. You must enter your name.');
+    alert('Sorry. You must enter your name, and it must be in UPPER case.');
 }
 
 function userCar() {
-  let haveCar = prompt ('Do you like your car?');
+  let haveCar = prompt ('Do you like your car?').toLowerCase();
   if(haveCar === "y" && haveCar === "n")
 
     alert('I like mine, ${name}.');
 
     else
 
-    alert('Invalid input. You have to type y or n.');
+    alert('Invalid input. You have to type y or n in lower case.');
 }
 
 function carColor() {
-  let color = prompt ('Do you have a light-colored car?');
+  let color = prompt ('Do you have a light-colored car?').toLowerCase();
   if(color === "y" && color === "n")
   
-      alert('Invalid input. You have to type y or n.');
+      alert('Invalid input. You have to type y or n in lower case.');
   
       else
 
@@ -32,10 +32,10 @@ function carColor() {
 }
 
 function newCar() {
-  let carAge = prompt ('Do you have a new car?');
+  let carAge = prompt ('Do you have a new car?').toUpperCase();
   if(carAge === "y" && carAge === "n")
   
-      alert('Invalid input. You have to type y or n.');
+      alert('Invalid input. You have to type y or n in UPPER case.');
   
       else
 
@@ -43,10 +43,10 @@ function newCar() {
 }
 
 function likeCar() {
-  let like = prompt ('Do you like your car?');
+  let like = prompt ('Do you like your car?').toLowerCase();
   if(like === "y" && like === "n")
   
-      alert('Invalid input. You have to type y or n.');
+      alert('Invalid input. You have to type y or n in lower case.');
   
       else
 
@@ -54,19 +54,19 @@ function likeCar() {
 }
 
 function dreamCar() {
-  let dream = prompt ('Do you like your car?');
+  let dream = prompt ('Do you like your car?').toUpperCase();
   if(dream === "y" && dream === "n")
   
-      alert('Invalid input. You have to type y or n.');
+      alert('Invalid input. You have to type y or n in UPPER case.');
   
       else
 
-      alert('My dream car is a hover car. Teehee!');
+      alert('My dream car is a hover car. Teehee! Thanks for playing my guessing game.');
 }
 
-let scent = bathScent();
-let water = waterTemp();
-let useCandles = bathCandles();
-let useBook = bathBook();
-let message = runABath(scent, water, useCandles, useBook);
-console.log(message);
+let name = userName();
+let haveCar = userCar();
+let color = carColor();
+let carAge = newCar();
+let like = likeCar();
+let dream = dreamCar();
