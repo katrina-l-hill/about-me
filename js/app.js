4436679313f7235cd9ec5e7656ccb0847a9f4096
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-09 17:56:04 -0800

    edited text for question prompt

--- js/app.js
+++ js/app.js
@@ -77,46 +77,116 @@
 // }
 
 let myNumber = 8;
-let userPrompt = prompt('Try to guess my favorite number. Hint: It\'s between 5 and 10.');
-console.log('User entered a number.')
-while (userGuess < myNumber) {
-  userPrompt = prompt('Nope. Your guess is too low. Try again.');
-while (userGuess > myNumber) {
-  userPrompt = promt('Nope. Your guess is too high.');
-}
-if (userGuess === myNumber) {
-  alert('Ding ding ding! You guessed correctly.');
-}
-if (userGuess > 4) {
-  alert('Sorry. You\'ve run out of chances to guess. The correct answer is 8.');
-}
-
-
-
-// function favNumber() {
-//   let myNumber = prompt('Try to guess my favorite number. Hint: It\'s between 5 and 10.');
-//   while (myNumber != '5' && myNumber != '6' && myNumber != '7' && myNumber != '9' && myNumber != '10') {
-//     myNumber = prompt('Nope. Not the right number.');
-//     let counter = 10;
-//     while (counter < 10);
-// }
-//   console.log('User entered a number.')
-//   alert('Nope. Not the right number.')
+// let numTry = 0;
+// while (numTry != 8); {
+//   console.log('Sorry. That\'s not my favorite number.');
+//   numTry++;
 // }
 
+for (let i = 0; i < 4; i++) {
+  console.log('Sorry. That\'s not my favorite number.');
+  while (i < 4) {
+    i++;
+  }
+  if (guess < myNumber) {
+      userPrompt = prompt('Nope. Your guess is too low. Try again.'); 
+  }
+  if (guess > myNumber) {
+    userPrompt = prompt('Nope. Your guess is too high.');
+  }
 
-// this is the teacher's code for reference
-// let foodsILike = ['tatertots', 'juanitas tortilla chips', 'popcorn', 'kale'];
-
-// for (let i = 0; i < foodsILike.length; i++) {
-//   console.log(foodsILike[i]);
+// let maxTries = 4;
+// let numTries = 1;
+// let myNumber = 8;
+// let userResponse = prompt("what number?")
+// while(userResponse != myNumber && numTries < maxTries)
+// {
+//   if(userResponse > myNumber)
+//   {
+//     userResponse = prompt("Too high. Try again.");
+//   }
+//   else if(userResponse < myNumber)
+//   {
+//     userResponse = prompt("Too low. Try again.");
+//   }
+//   numTries++;
+// }
+// if(numTries < maxTries)
+// {
+//   alert("way to go");
+// }
+// else
+// {
+//   alert("game over");
 // }
 
-// let name = userName();
-// let haveVehicle = userVehicle();
-// let color = vehicleColor();
-// let vehicleAge = newVehicle();
-// let like = likeVehicle();
-// let dream = dreamVehicle(name);
-// let myNumber = favNumber();
-let guess = userGuess();
+// let stuff = ['car', 'mouse', 'bird'];
+// for(let j = 0; j < stuff.length; j++){
+//   let item = stuff[j];
+// }
+
+// let j2 = 0;
+// while(j2  < 10)
+// {
+//   j2++;
+// }
+
+  // while (userGuess < myNumber) {
+  //   userPrompt = prompt('Nope. Your guess is too low. Try again.');
+  // while (userGuess > myNumber) {
+  //     userPrompt = prompt('Nope. Your guess is too high.');
+  //   }
+  // if (userGuess < i)
+  //     userPrompt = prompt('Nope. Your guess is too low. Try again.');
+  // if (userGuess > i)
+  //     userPrompt = prompt('Nope. Your guess is too high.');
+  // }
+  // if (userGuess === 8) {
+  //   alert('Ding ding ding! You guessed correctly.');
+  // }
+
+
+
+  // let userPrompt = prompt('Try to guess my favorite number. Hint: It\'s between 5 and 10.');
+  // console.log('User entered a number.')
+  // while (userGuess < myNumber) {
+  //   userPrompt = prompt('Nope. Your guess is too low. Try again.');
+  // while (userGuess > myNumber) {
+  //   userPrompt = prompt('Nope. Your guess is too high.');
+  // }
+  // if (userGuess === myNumber) {
+  //   alert('Ding ding ding! You guessed correctly.');
+  // }
+  // if (userGuess > 4) {
+  //   alert('Sorry. You\'ve run out of chances to guess. The correct answer is 8.');
+  // }
+
+
+
+  // function favNumber() {
+  //   let myNumber = prompt('Try to guess my favorite number. Hint: It\'s between 5 and 10.');
+  //   while (myNumber != '5' && myNumber != '6' && myNumber != '7' && myNumber != '9' && myNumber != '10') {
+  //     myNumber = prompt('Nope. Not the right number.');
+  //     let counter = 10;
+  //     while (counter < 10);
+  // }
+  //   console.log('User entered a number.')
+  //   alert('Nope. Not the right number.')
+  // }
+
+
+  // this is the teacher's code for reference
+  // let foodsILike = ['tatertots', 'juanitas tortilla chips', 'popcorn', 'kale'];
+
+  // for (let i = 0; i < foodsILike.length; i++) {
+  //   console.log(foodsILike[i]);
+  // }
+
+  // let name = userName();
+  // let haveVehicle = userVehicle();
+  // let color = vehicleColor();
+  // let vehicleAge = newVehicle();
+  // let like = likeVehicle();
+  // let dream = dreamVehicle(name);
+  let myNumber = favNumber();
+  let guess = userGuess();
